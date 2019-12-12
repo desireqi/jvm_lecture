35ce93d9d225d6273611aae879f511e53aad551f
Xu Qi <xuqi@xiaobangtouzi.com>
2019-12-12 13:34:09 +0800

    jvm_lecture

---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest5.java
@@ -0,0 +1,20 @@
+package com.shengsiyuan.jvm.classloader;
+
+public class MyTest5 {
+
+    public static void main(String[] args) {
+        System.out.println(MyChild5.b);
+    }
+}
+
+interface MyParent5{
+
+    public static int a = 5;
+
+}
+
+interface MyChild5 extends MyParent5{
+
+    public static int b = 6;
+
+}
