71622c45c46ca11593102dd15bf8af69800ddc5d
Xu Qi <xuqi@xiaobangtouzi.com>
2019-12-16 13:59:49 +0800

    jvm_lecture

---
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest11.java
@@ -0,0 +1,26 @@
+package com.shengsiyuan.jvm.classloader;
+
+class Parent3{
+    static int a = 3;
+    static {
+        System.out.println("Parent static block");
+    }
+    static void doSomething(){
+        System.out.println("do some thing");
+    }
+}
+
+class Child3 extends Parent3{
+    static {
+        System.out.println("Child3 static block");
+    }
+}
+
+
+
+public class MyTest11 {
+    public static void main(String[] args) {
+        System.out.println(Child3.a);
+        Child3.doSomething();
+    }
+}
